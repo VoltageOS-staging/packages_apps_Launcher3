930be198eb4ac974077f5d99fda28ac7f67e770d
Pranav Vashi <neobuddy89@gmail.com>
2022-11-13 13:50:05 +0530

    Launcher3: Switch to icon pack theming preview
    Ref:
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/8155a4f3db5888b578aecb8904529a22f962968e
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/174d11c6fa88e85920ec078af506fc56ef3bccd4
    
    Tweaked to our usage
    
    Change-Id: I82b16d0d4bf5f8e9ef1dca3dc64e4aa1714e04f1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -66,6 +66,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.annotation.XmlRes;
 
 import com.android.launcher3.config.FeatureFlags;
+import com.android.launcher3.customization.IconDatabase;
 import com.android.launcher3.dagger.ApplicationContext;
 import com.android.launcher3.dagger.LauncherAppComponent;
 import com.android.launcher3.dagger.LauncherAppSingleton;
@@ -169,6 +170,7 @@ public class InvariantDeviceProfile {
     public int[] numFolderColumns;
     public float[] iconSize;
     public float[] iconTextSize;
+    public String iconPack;
     public int iconBitmapSize;
     public int fillResIconDpi;
     public @DeviceType int deviceType;
@@ -327,7 +329,8 @@ public class InvariantDeviceProfile {
                     ICON_SIZE.getSharedPrefKey().equals(key) ||
                     FONT_SIZE.getSharedPrefKey().equals(key) ||
                     ENABLE_TWOLINE_ALLAPPS_TOGGLE.getSharedPrefKey().equals(key) ||
-                    ROW_HEIGHT.getSharedPrefKey().equals(key)) {
+                    ROW_HEIGHT.getSharedPrefKey().equals(key) ||
+                    IconDatabase.KEY_ICON_PACK.equals(key)) {
                 onConfigChanged();
             }
         };
@@ -436,6 +439,7 @@ public class InvariantDeviceProfile {
         for (int i = 1; i < iconSize.length; i++) {
             maxIconSize = Math.max(maxIconSize, iconSize[i]);
         }
+        iconPack = IconDatabase.getGlobal(context);
         iconBitmapSize = ResourceUtils.pxFromDp(maxIconSize, metrics);
 
         fillResIconDpi = getLauncherIconDensity(iconBitmapSize);
@@ -551,7 +555,8 @@ public class InvariantDeviceProfile {
     private Object[] toModelState() {
         return new Object[]{
                 numColumns, numRows, numSearchContainerColumns, numDatabaseHotseatIcons,
-                iconBitmapSize, fillResIconDpi, numDatabaseAllAppsColumns, dbFile, mLocale};
+                iconPack, iconBitmapSize, fillResIconDpi, numDatabaseAllAppsColumns,
+                dbFile, mLocale};
     }
 
     /** Updates IDP using the provided context. Notifies listeners of change. */
