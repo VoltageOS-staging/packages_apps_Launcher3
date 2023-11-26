144380087bee1d9493c4a0a523110957016c2560
Pranav Vashi <neobuddy89@gmail.com>
2023-11-26 20:33:21 +0530

    Launcher3: Quickspace: Make weather condition text optional
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -458,7 +458,8 @@ public class Launcher extends StatefulActivity<LauncherState>
          	    case Utilities.KEY_SHOW_QUICKSPACE_NOWPLAYING:
          	    case Utilities.KEY_SHOW_QUICKSPACE_WEATHER:
          	    case Utilities.KEY_SHOW_QUICKSPACE_PSONALITY:
-	            case Utilities.KEY_QUICKSPACE_SHOW_CITY:
+	            case Utilities.KEY_SHOW_QUICKSPACE_WEATHER_CITY:
+	            case Utilities.KEY_SHOW_QUICKSPACE_WEATHER_TEXT:
                         mNeedsRestart = true;
                         break;
                     default:
