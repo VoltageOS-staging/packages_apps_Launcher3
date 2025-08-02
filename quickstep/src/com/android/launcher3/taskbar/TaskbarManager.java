RisingOS-Revived/android_packages_apps_Launcher3

Launcher3: Implement 3-btn-nav layout settings [2/3]
[neobuddy89]
Use mOnTaskBarChangeListener for launcher restart.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -121,6 +121,9 @@ public class TaskbarManager {
     public static final Uri NAV_BAR_INVERSE = Settings.Secure.getUriFor(
             Settings.Secure.NAVBAR_INVERSE_LAYOUT);
 
+    public static final Uri NAV_BAR_LAYOUT = Settings.Secure.getUriFor(
+            Settings.Secure.NAVBAR_LAYOUT_MODE);
+
     public static final Uri ENABLE_TASKBAR = LineageSettings.System.getUriFor(
             LineageSettings.System.ENABLE_TASKBAR);
 
@@ -275,6 +278,8 @@ public class TaskbarManager {
                 .register(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .register(NAV_BAR_INVERSE, mOnSettingsChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .register(NAV_BAR_LAYOUT, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .register(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
@@ -759,6 +764,8 @@ public class TaskbarManager {
                 .unregister(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .unregister(NAV_BAR_INVERSE, mOnSettingsChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .unregister(NAV_BAR_LAYOUT, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .unregister(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)

VoltageOS-staging/packages_apps_Launcher3

Signed-off-by: Dmitrii <bankersenator@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -148,6 +148,9 @@ public class TaskbarManager implements DisplayDecorationListener {
     public static final Uri ENABLE_TASKBAR = Settings.System.getUriFor(
             Settings.System.ENABLE_TASKBAR);
 
+    public static final Uri NAV_BAR_LAYOUT = Settings.Secure.getUriFor(
+            Settings.Secure.NAVBAR_LAYOUT_MODE);
+
     public static final Uri NAVIGATION_BAR_HINT = Settings.System.getUriFor(
             Settings.System.NAVIGATION_BAR_HINT);
 
@@ -479,6 +482,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .register(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .register(NAV_BAR_INVERSE, mOnSettingsChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .register(NAV_BAR_LAYOUT, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .register(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
@@ -1142,6 +1147,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .unregister(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .unregister(NAV_BAR_INVERSE, mOnSettingsChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .unregister(NAV_BAR_LAYOUT, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .unregister(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)