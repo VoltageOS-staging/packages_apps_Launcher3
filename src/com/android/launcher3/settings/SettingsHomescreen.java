93e227325427b94328f2f967ef938853d44ac05f
Dmitrii <bankersenator@gmail.com>
2025-08-06 18:20:21 +0000

    Quickspace: implement voltage style
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -123,7 +123,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
                 LauncherPrefs.SHORT_PARALLAX.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SINGLE_PAGE_CENTER.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.SHOW_QUICKSPACE_ALT.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.QUICKSPACE_UI_STYLE.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_PSONALITY.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_NOWPLAYING.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER.getSharedPrefKey().equals(key) ||
