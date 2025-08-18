b8f73287a31020e4372ac3c1e1376b7ca4d74e00
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-18 16:48:59 +0800

    [PERF] remove overview vibration haptics
    Change-Id: I921265e16df58841b7e8d8ac643336179d421ad7
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -180,7 +180,6 @@ import com.android.launcher3.util.SplitConfigurationOptions.StagePosition;
 import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.TraceHelper;
 import com.android.launcher3.util.TranslateEdgeEffect;
-import com.android.launcher3.util.VibratorWrapper;
 import com.android.launcher3.util.ViewPool;
 import com.android.launcher3.util.coroutines.DispatcherProvider;
 import com.android.launcher3.util.window.WindowManagerProxy.DesktopVisibilityListener;
@@ -1849,21 +1848,10 @@ public abstract class RecentsView<
 
     @Override
     protected void onEdgeAbsorbingScroll() {
-        vibrateForScroll();
     }
 
     @Override
     protected void onScrollOverPageChanged() {
-        vibrateForScroll();
-    }
-
-    private void vibrateForScroll() {
-        long now = SystemClock.uptimeMillis();
-        if (now - mScrollLastHapticTimestamp > mScrollHapticMinGapMillis) {
-            mScrollLastHapticTimestamp = now;
-            VibratorWrapper.INSTANCE.get(mContext).vibrate(SCROLL_VIBRATION_PRIMITIVE,
-                    SCROLL_VIBRATION_PRIMITIVE_SCALE, SCROLL_VIBRATION_FALLBACK);
-        }
     }
 
     @Override
