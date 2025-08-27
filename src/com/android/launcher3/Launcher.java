3ed90e758d2cb919c16ca6eaad25f66effa8c853
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-27 08:03:21 +0800

    Launcher3: cpu core binding opt
    based from transsion launcher boost
    
    Change-Id: I5e4324f41a422525e7a1d21adf02808842beaa49
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -135,6 +135,7 @@ import android.graphics.RectF;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.os.Process;
 import android.os.StrictMode;
 import android.os.SystemClock;
 import android.os.Trace;
@@ -172,6 +173,7 @@ import androidx.core.os.BuildCompat;
 import androidx.window.embedding.RuleController;
 
 import com.android.app.animation.Animations;
+import com.android.internal.util.BoostHelper;
 
 import com.android.launcher3.DropTarget.DragObject;
 import com.android.launcher3.accessibility.LauncherAccessibilityDelegate;
@@ -1346,6 +1348,7 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
+        BoostHelper.setThreadAffinity(Process.myPid(), 0);
     }
 
     @Override
@@ -1365,6 +1368,7 @@ public class Launcher extends StatefulActivity<LauncherState>
             mQuickSpace.onPause();
         }
         mAppWidgetHolder.setActivityResumed(false);
+        BoostHelper.setThreadAffinity(Process.myPid(), 1);
     }
 
     /**
